1e33fcf6e1177005800a3a4ae589cc8c4dc7bdc0
1Deslre <2135232170@qq.com>
2024-07-09 23:25:41 +0800

    新增添加对应编号查询controller

---
+++ DrawingNode/src/main/java/org/deslre/nodeModule/controller/OperationController.java
@@ -0,0 +1,31 @@
+package org.deslre.nodeModule.controller;
+
+import org.deslre.nodeModule.service.RelationService;
+import org.deslre.utils.ResultCodeEnum;
+import org.deslre.utils.Results;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+import static org.deslre.utils.StringUtil.isEmpty;
+
+@RestController
+@RequestMapping("/operation")
+public class OperationController {
+
+    @Resource
+    private RelationService relationService;
+
+    @GetMapping("/inquire/{case}")
+    public Results<String> addAllCorrespondingCases(@PathVariable("case") String caseNumber) {
+        if (isEmpty(caseNumber)) {
+            return Results.fail(ResultCodeEnum.EMPTY_VALUE);
+        }
+        return relationService.addAllCorrespondingCases(caseNumber);
+    }
+
+
+}
