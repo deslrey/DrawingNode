122c5f579d4f7c8c4294c69c7e646c9606abc3d8
deslre <2135232170@qq.com>
2024-07-14 19:10:17 +0800

    初始化前后端分离后端项目模块

---
+++ SeparateBackend/src/main/java/org/deslre/SeparateBackendApplication.java
@@ -0,0 +1,13 @@
+package org.deslre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeparateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeparateBackendApplication.class, args);
+    }
+
+}
